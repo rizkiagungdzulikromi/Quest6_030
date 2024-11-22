bfec3ba6dde1ea5d2b1a640ccc256ed20ac5b34a
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-11-22 16:04:22 +0700

    membuat fun tampilv

--- app/src/main/java/com/example/pertemuan7/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/pertemuan7/ui/view/screen/TampilData.kt
@@ -1,2 +1,39 @@
 package com.example.pertemuan7.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan7.R
+import com.example.pertemuan7.model.Mahasiswa
+import com.example.pertemuan7.model.RencanaStudi
+
+//@Preview(showBackground = true)
+@Composable
+fun TampilView(
+    mahasiswa: Mahasiswa,
+    krs: RencanaStudi,
+    onbackbuttonClicked: () -> Unit,
+    onResetButtonClicked: () -> Unit,
+)
+}
