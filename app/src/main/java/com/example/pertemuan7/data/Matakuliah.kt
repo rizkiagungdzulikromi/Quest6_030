3e53d85b826c50b4013022227cb79a2db80deb3e
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-11-22 15:51:47 +0700

    Menambah isi dari kotlin matakuliah

---
+++ app/src/main/java/com/example/pertemuan7/data/Matakuliah.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan7.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pemrograman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasaran Buatan",
+        "Computer Vision"
+    )
+}
---
+++ app/src/main/java/com/example/pertemuan7/navigation/PengelolaHalaman.kt
@@ -0,0 +1,2 @@
+package com.example.pertemuan7.navigation
+
